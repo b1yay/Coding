6ac8b9c61ff4d48f264d3c7a93cca761fd20561a
b1yay <144415052+b1yay@users.noreply.github.com>
2023-10-03 00:12:08 -0700

    Add files via upload

---
+++ week5/t2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main() {
+    float base, exponent;
+    cout << "Enter the base number: ";
+    cin >> base;
+    cout << "Enter the exponent: ";
+    cin >> exponent;
+    cout << base << " raised to the power " << exponent << " is: " << pow(base, exponent);
+}
